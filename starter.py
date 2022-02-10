e15da5bc0239054ee241c28b4a0def84e01501d8
Angel Hernandez <angelhernandez2021@u.northwestern.edu>
2022-02-09 20:35:25 -0600

    step 4a

--- starter.py
+++ starter.py
@@ -43,18 +43,18 @@ frank = (
 # helper function that draws a grid.
 make_grid(canvas, 600, 600)
 
-def draw_row(canvas:Canvas, row:tuple, top_left:tuple, pixel:int=25):
+def draw_row(canvas:Canvas, row:tuple, top_left:tuple, colors:tuple=('pink', 'black', 'blue'), pixel:int=25):
     x = top_left[0]
     y = top_left[1]
     for cell in row:
         if cell == 0:
             make_square(canvas, (x, y), pixel, fill_color='white')
         elif cell == 1:
-            make_square(canvas, (x, y), pixel, fill_color='pink')
+            make_square(canvas, (x, y), pixel, fill_color=colors[0])
         elif cell == 2:
-            make_square(canvas, (x, y), pixel, fill_color='black')
+            make_square(canvas, (x, y), pixel, fill_color=colors[1])
         elif cell == 3:
-            make_square(canvas, (x, y), pixel, fill_color='blue')
+            make_square(canvas, (x, y), pixel, fill_color=colors[2])
         else:
             print('Only supports up to 3 colors.')
         x += pixel
@@ -75,12 +75,13 @@ for row in heart:
     y += 20
 
 # create function:
-def draw_pixel_art(canvas:Canvas, top_left:tuple, grid:tuple, pixel:int=10):
+def draw_pixel_art(canvas:Canvas, top_left:tuple, grid:tuple, colors:tuple=('pink', 'black', 'blue'), pixel:int=10):
     x = top_left[0]
     y = top_left[1]
     for row in grid:
-        draw_row(canvas, row, (x, y), pixel=pixel)
+        draw_row(canvas, row, (x, y), colors, pixel=pixel)
         y += pixel
+draw_pixel_art(canvas, (350, 115), heart, colors=('black', 'black', 'black'), pixel=5)
 
 ########################## YOUR CODE ABOVE THIS LINE ############################## 
 canvas.mainloop()
